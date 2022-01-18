d0d326ca8905ed9e03c238ca31304d24a7e9c4bb
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-01-17 21:11:39 -0800

    Add card matching logic and card flip transitions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png"},
-  { "src": "/img/potion-1.png"},
-  { "src": "/img/ring-1.png"},
-  { "src": "/img/scroll-1.png"},
-  { "src": "/img/shield-1.png"},
-  { "src": "/img/sword-1.png"}
-]
-
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
+];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   const shuffleCards = () => {
     // spreads cardImages array twice to duplicate cards
@@ -21,13 +24,52 @@ function App() {
       // Fires for each item pair, if negative number order remains same, swaps if positive number
       .sort(() => Math.random() - 0.5)
       // Add id property for each item in sorted array
-      .map((card) => ({ ...card, id: Math.random() }))
+      .map((card) => ({ ...card, id: Math.random() }));
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
 
-      setCards(shuffledCards);
-      setTurns(0);
-  }
+  // handle card choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
-  console.log(cards, turns);
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true); // runs first time useEffect so must be here
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        console.log('no match');
+        setTimeout(() => resetTurn(), 800);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // rest choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  // start game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="App">
@@ -35,16 +77,17 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front" />
-              <img className="cover"  src="/img/cover.png" alt="card cover" />
-            </div>
-          </div>
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
-      
+      <p>Turns: {turns}</p>
     </div>
   );
 }
